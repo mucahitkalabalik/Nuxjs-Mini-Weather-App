e78737d7cf435090cccdf92c7461d65abcb9210d
Mücahit <71331160+mucahitkalabalik@users.noreply.github.com>
2022-01-30 05:14:24 +0300

    Add files via upload

--- components/appheader.vue
+++ components/appheader.vue
@@ -1,8 +1,10 @@
 <template>
-  <v-app-bar max-height="70" class="d-flex justify-center align-center">
+  <v-app-bar max-height="70" >
+    <v-img class="d-flex justify-center align-center white--text px-15" src="https://images.unsplash.com/photo-1561485132-59468cd0b553?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1952&q=80" max-height="70">
       <v-toolbar-title>
           Hava Durumu Uygulaması
       </v-toolbar-title>
+      </v-img>
   </v-app-bar>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,29 @@
 <template>
-  <v-row class="d-flex align-center justify-center">
+  <v-row class="d-flex align-center justify-center bgcolor">
     <v-col lg="5" md="5" class="d-flex align-center justify-center">
       <v-card
         min-height="500"
         min-width="100%"
-        class="d-flex align-center justify-center bg2"
+        class="d-flex align-center justify-center rounded-xl"
       >
-        <v-text-field
-          v-model="city"
-          class="elevation-14 ml-5 pl-5 pt-7"
-          label="Şehir Adını Girin.."
+        <v-img
+          src="https://images.unsplash.com/photo-1630260643564-7f9c9c140682?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          class="d-flex align-center justify-center card-img2 "
+          max-height="500"
+          
         >
-        </v-text-field>
-        <v-btn @click="getWeather" class="ml-5 mr-5 elevation-14" outlined
-          >Güncelle</v-btn
-        >
+          <v-text-field
+            v-model="city"
+            class="elevation-14 rounded-xl mx-5 pl-5 pt-7 mb-10 "
+            color="yellow darken-4 "
+            label="Şehir Adını Girin.."
+            
+          >
+          </v-text-field>
+          <v-btn @click="getWeather" class="ml-5 mr-5 elevation-14" outlined
+            >Güncelle</v-btn
+          >
+        </v-img>
       </v-card>
     </v-col>
     <v-col lg="5" md="5" class="d-flex align-center justify-center mr-15">
@@ -22,22 +31,25 @@
         v-if="weather"
         min-height="500"
         min-width="100%"
-        class="d-flex align-center justify-center flex-column bg"
+        class="d-flex align-center justify-center flex-column rounded-xl"
       >
-        <v-img :src="selectedimages" max-height="90" max-width="100"></v-img>
-        <v-card-title> {{ weather.name }} </v-card-title>
-        <v-card-subtitle>
-          Hava Durumu : {{ weather.weather[0].description }}
-        </v-card-subtitle>
-        <v-card-subtitle>
-          Sıcaklık : {{ Math.ceil(weather.main.temp) }}
-        </v-card-subtitle>
-        <v-card-subtitle>
-          Maksimum Sıcaklık : {{ Math.ceil(weather.main.temp_max) }}
-        </v-card-subtitle>
-        <v-card-subtitle>
-          Minimum Sıcaklık : {{ Math.ceil(weather.main.temp_min) }}
-        </v-card-subtitle>
+        <v-img class="card-img1" :src="selectedimages || 'https://images.unsplash.com/photo-1630260667842-830a17d12ec9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'" max-height="500" min-width="100%">
+          <div class="d-flex align-center justify-center flex-column container-card">
+            <v-card-title class="white--text"> {{ weather.name }} </v-card-title>
+            <v-card-subtitle class="white--text">
+              Hava Durumu : {{ weather.weather[0].description }}
+            </v-card-subtitle>
+            <v-card-subtitle class="white--text">
+              Sıcaklık : {{ Math.ceil(weather.main.temp) }} 'C
+            </v-card-subtitle>
+            <v-card-subtitle class="white--text">
+              Maksimum Sıcaklık : {{ Math.ceil(weather.main.temp_max) }} 'C
+            </v-card-subtitle>
+            <v-card-subtitle class="white--text">
+              Minimum Sıcaklık : {{ Math.ceil(weather.main.temp_min) }} 'C
+            </v-card-subtitle>
+          </div>
+        </v-img>
       </v-card>
       <v-card
         v-else
@@ -63,14 +75,15 @@ export default {
       lang: "tr",
       images: {
         sunny:
-          "https://img-fotki.yandex.ru/get/370378/493212545.712/0_20c238_21b093fa_L",
+          "https://images.unsplash.com/photo-1604228741406-3faa38f4907a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=882&q=80",
         rainy:
-          "https://jf-staeulalia.pt/img/other/31/collection-animated-cloud-pictures-13.png",
+          "https://images.unsplash.com/photo-1573553557077-4cda64089870?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
         cloudy:
-          "https://jf-staeulalia.pt/img/other/31/collection-animated-cloud-pictures-25.png",
+          "https://images.unsplash.com/photo-1542549808-283c61ce5b0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
         snowy:
-          "https://e7.pngegg.com/pngimages/711/812/png-clipart-white-clouds-illustration-cloud-snow-weather-forecasting-snowy-weather-blue-winter.png",
-        smoke:"https://w7.pngwing.com/pngs/82/829/png-transparent-computer-icons-weather-fog-foggy-text-cloud-hand.png"
+          "https://images.unsplash.com/photo-1635972904366-3fb9ac369ca0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1467&q=80",
+        smoke:
+          "https://images.unsplash.com/photo-1635061285225-8bffdb7f45a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
       },
       selectedimages: null,
     };
@@ -83,7 +96,7 @@ export default {
       return new Promise((resolve, reject) => {
         console.log(this.city);
         axios
-           .get(`https://api.openweathermap.org/data/2.5/weather?`, {
+          .get(`https://api.openweathermap.org/data/2.5/weather?`, {
             params: {
               q: this.city,
               appid: this.appid,
@@ -101,10 +114,10 @@ export default {
               case "kar yağışlı" && "hafif kar yağışlı":
                 this.selectedimages = this.images.snowy;
                 break;
-              case "bulutlu" && "parçalı bulutlu":
+              case "bulutlu" && "parçalı bulutlu" && "kapalı":
                 this.selectedimages = this.images.cloudy;
                 break;
-              case "yağmurlu":
+              case "yağmurlu" && "hafif yağmur":
                 this.selectedimages = this.images.rainy;
                 break;
               case "sisli":
@@ -122,7 +135,7 @@ export default {
     },
     getWeather() {
       this.weather = null;
-      this.selectedimages=null;
+      this.selectedimages = null;
       return new Promise((resolve, reject) => {
         axios
           .get(`https://api.openweathermap.org/data/2.5/weather?`, {
@@ -143,16 +156,16 @@ export default {
               case "hafif kar yağışlı":
                 this.selectedimages = this.images.snowy;
                 break;
-                 case "kar yağışlı":
+              case "kar yağışlı":
                 this.selectedimages = this.images.snowy;
                 break;
-              case "bulutlu" && "parçalı bulutlu":
+              case "bulutlu" && "parçalı bulutlu" && "kapalı" && "az bulutlu":
                 this.selectedimages = this.images.cloudy;
                 break;
               case "yağmurlu":
                 this.selectedimages = this.images.rainy;
                 break;
-               case "sisli":
+              case "sisli":
                 this.selectedimages = this.images.smoke;
                 break;
               default:
@@ -167,10 +180,26 @@ export default {
 </script>
 
 <style>
-.bg {
-  background-color: rgba(19, 63, 104, 0.445) !important;
+.bgcolor{
+  background-color: rgba(141, 141, 141, 0.432);
 }
-.bg2 {
-  background-color: rgba(50, 113, 172, 0.473) !important;
+.theme--dark.v-input input, .theme--dark.v-input textarea {
+    color: rgb(233, 124, 0);
 }
+.theme--dark.v-label {
+    color: rgb(233, 124, 0);
+}
+.container-card{
+  background-color:rgba(0, 0, 0, 0.637);
+  min-width: 100%;
+  min-height: 100%;
+  margin: auto;
+}
+.card-img1{
+box-shadow: rgba(255, 255, 255, 0.4) 5px 5px, rgba(255, 255, 255, 0.3) 10px 10px, rgba(226, 226, 226, 0.2) 15px 15px, rgba(255, 255, 255, 0.1) 20px 20px, rgba(255, 255, 255, 0.05) 25px 25px;
+}
+.card-img2{
+box-shadow: rgba(255, 255, 255, 0.4) -5px 5px, rgba(255, 255, 255, 0.3) -10px 10px, rgba(255, 255, 255, 0.2) -15px 15px, rgba(255, 255, 255, 0.1) -20px 20px, rgba(255, 255, 255, 0.05) -25px 25px;
+}
+
 </style>
